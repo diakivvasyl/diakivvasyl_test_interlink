4d1b53e853d7ca2194784b7ebe7e8e5bc3df9a30
diakivvasyl <diakivvasyl@gmail.com>
2018-07-31 18:50:19 +0300

    Added Student and Knowledge classes.

--- src/person/Student.java
+++ src/person/Student.java
@@ -3,11 +3,26 @@ package person;
 import person.consciousness.Knowledge;
 
 public class Student {
+    //name of student
+    private String name;
+    //knowledge of student
+    private Knowledge Know;
+    //construct student
     public Student(String name) {
-        //TODO: Implementation is needed
+        this.name=name;
+        this.Know=new Knowledge(0);
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+    //setters
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setKnowledge(Knowledge knowledge) {
-        //TODO: Implementation is needed
+        this.Know.setLevel(knowledge.getLevel());
     }
 }
